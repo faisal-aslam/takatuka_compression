52f4098a1ff3570a613c00c8fe1d6db579d4af2b
faisal-aslam <faisal.aslam@gmail.com>
2025-04-01 23:46:18 +0500

    forgot to add src/constant.h.

---
+++ src/constants.h
@@ -0,0 +1,18 @@
+#define SEQ_LENGTH_START 2
+#define SEQ_LENGTH_LIMIT 4 //a.k.a k
+#define BLOCK_SIZE (1 << 20)
+#define LEAST_REDUCTION 8
+
+// Group definitions
+#define GROUP1_THRESHOLD 16
+#define GROUP2_THRESHOLD 32
+#define GROUP3_THRESHOLD 48
+#define GROUP4_THRESHOLD 4144
+#define MAX_NUMBER_OF_SEQUENCES 4144 //a.k.a m
+
+// Codeword sizes
+#define GROUP1_CODE_SIZE 7
+#define GROUP2_CODE_SIZE 7
+#define GROUP3_CODE_SIZE 7
+#define GROUP4_CODE_SIZE 15
+
