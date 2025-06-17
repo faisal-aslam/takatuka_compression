9345f73c35a2cd480f3c08db17b9a81f24382eee
faisal-aslam <faisal.aslam@gmail.com>
2025-06-17 10:36:18 +0500

    created logic.c/h. Move key logic of the algorithm there. The main.c has now only the main function.

---
+++ src/logic.h
@@ -0,0 +1,7 @@
+#ifndef LOGIC_H
+#define LOGIC_H
+
+#include <stdint.h>
+void processBlock(const uint8_t *block, uint32_t block_size);
+
+#endif
--- used_sources.mk
+++ used_sources.mk
@@ -1,6 +1,7 @@
 # this is used_sources.mk which lists all actually used .c source files
 SRCS = \
     src/main.c \
+    src/logic.c \
     src/decompress/decompress.c \
     src/graph/graph_visualizer.c \
     src/map/xxhash.c \
