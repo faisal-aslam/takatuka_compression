a4b10048ca70d02574248ff575e90482dbe2fb79
faisal-aslam <faisal.aslam@gmail.com>
2025-06-22 13:13:14 +0500

    Made changes so that it should be able to run 1000 bytes file. This will be used for testing before moving to bigger file.

--- src/constants.h
+++ src/constants.h
@@ -11,8 +11,8 @@
 #endif
 
 #define SEQ_LENGTH_START 2
-#define SEQ_LENGTH_LIMIT 16 //a.k.a k
-#define BLOCK_SIZE 2000
+#define SEQ_LENGTH_LIMIT 64 //a.k.a k
+#define BLOCK_SIZE 1100
 
 #define TOTAL_GROUPS 4
 
