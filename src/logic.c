b0fee1d4bb7e7572db77d91b919c43f993f161bc
faisal-aslam <faisal.aslam@gmail.com>
2025-07-06 18:47:46 +0500

    Make the creation of graph very simple, leaving graph optimizations for later.

--- src/logic.c
+++ src/logic.c
@@ -7,7 +7,6 @@
 #include "graph/shortest_path.h"
 #include "map/sequence_repository_useless.h"
 
-SequenceRepository useless_repo;
 
 #define MIN(a,b) ((a) < (b) ? (a) : (b))
 #define MAX(a,b) ((a) > (b) ? (a) : (b))
@@ -20,19 +19,9 @@ static inline void create_root() {
     assert(root_node != NULL);
 }
 
-static GraphNode* create_node(uint32_t start, uint8_t length) {
-    GraphNode* node = get_next_node();    
-    if (!node) return NULL;
-    
-    node->offset = start;
-    node->sequence_length = length;
-    return node;
-}
-
 void process_block(const uint8_t *block, uint32_t block_size) {
     init_graph();
     create_root();
-    seq_repo_init(&useless_repo, INITIAL_CAPACITY);
 
 #ifdef DEBUG
     print_graph_node(get_graph_node(0));
@@ -49,23 +38,10 @@ void process_block(const uint8_t *block, uint32_t block_size) {
         // Make sequences of specific sizes. 
         for (uint8_t seq_len = 1; seq_len <=  max_sequence; seq_len++) {
             uint32_t start = block_index - seq_len + 1;
-            GraphNode *node = create_node(start, seq_len);
-
-            if (seq_len > 1) {
-                uint32_t node_id = seq_repo_get_node_id(&useless_repo, &block[start], seq_len);
-                node->isUseless = 1; // Assume useless initially
-
-                if (node_id != UINT32_MAX) {
-                    GraphNode *g_node = get_graph_node(node_id);
-                    if (g_node) g_node->isUseless = 0; // Mark existing node as useful
-                    node->isUseless = 0;                // Mark current node as useful
-                } else {
-                    seq_repo_add(&useless_repo, &block[start], seq_len, node->node_id);
-                    // Still marked useless until repeated
-                }
-            } else {
-                // 1-length sequences are always useful and not tracked
-                node->isUseless = 0;
+            GraphNode* node = get_next_node();    
+            if (node) {    
+                node->offset = start;
+                node->sequence_length = seq_len;
             }
 
 #ifdef DEBUG
