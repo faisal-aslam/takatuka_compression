ddaf9e85a0711a7afb19f7a6163695981686dfd4
faisal-aslam <faisal.aslam@gmail.com>
2025-07-19 15:03:29 +0500

    RLE nodes are now created correctly at the correct level.

--- src/logic.c
+++ src/logic.c
@@ -20,6 +20,15 @@ static inline void create_root() {
     root_node = get_next_node();
     assert(root_node != NULL);
 }
+// use to store
+typedef struct {
+    uint32_t RLE_offset;       // The starting point of block of RLE
+    uint16_t next_RLE_level;   // we must wait till the next RLE level to create the node.
+    uint8_t repeat_seq_length; // number of bytes repeated. Like for ABCABCABCABC repeat_seq_length=3
+    uint8_t length_of_RLE;     // length of RLE sequence. Like for ABCABCABCABC length_of_RLE=4
+} RLE_info;
+
+RLE_info rle_info;
 
 static inline GraphNode *create_node(uint32_t start, uint8_t length) {
     GraphNode *node = get_next_node();
@@ -28,6 +37,31 @@ static inline GraphNode *create_node(uint32_t start, uint8_t length) {
     node->offset = start;
     node->sequence_length = length;
     return node;
+}
+
+static void RLE_logic(const uint8_t *block, uint32_t block_index, uint32_t block_size) {
+    // Create RLE node, if any. There could be at most one RLE node per level.
+    uint16_t current_level = get_last_level_index();
+    if (rle_info.next_RLE_level < current_level &&
+        is_RLE_sequence(&rle_info.repeat_seq_length, &rle_info.length_of_RLE, MIN(block_size, 255), block_index,
+                        block)) {
+        // wait for the right level to create node.
+        // do not create any RLE nodes before reaching that level.
+        // remember data of RLE node to be created later on on the appropriate level.
+        rle_info.next_RLE_level = get_last_level_index() + rle_info.length_of_RLE -1;
+        rle_info.RLE_offset = block_index;
+        return;
+    }
+    GraphNode* current_node;
+    if (current_level == rle_info.next_RLE_level) {
+        current_node = create_node(rle_info.RLE_offset, rle_info.length_of_RLE);
+        current_node->is_RLE = 1;
+        current_node->repeat_seq_length = rle_info.repeat_seq_length;
+        current_node->length_of_RLE = rle_info.length_of_RLE;
+#ifdef DEBUG
+        print_graph_node(current_node); // print the RLE node.
+#endif
+    }
 }
 
 void process_block(const uint8_t *block, uint32_t block_size) {
@@ -56,6 +90,7 @@ void process_block(const uint8_t *block, uint32_t block_size) {
             print_graph_node(current_node); // print the newly create node.
 #endif
         }
+        RLE_logic(block, block_index, block_size);
 
 #ifdef DEBUG
         visualize_graph(block); // create graph in DOT for visualization.
