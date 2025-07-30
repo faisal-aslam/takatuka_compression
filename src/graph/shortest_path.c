0133de8b6887d46a96f7c035485686e2c9105d06
faisal-aslam <faisal.aslam@gmail.com>
2025-07-30 20:49:55 +0500

    do not change freq during shortest path calculation. It is slow, and harmful.

--- src/graph/shortest_path.c
+++ src/graph/shortest_path.c
@@ -214,13 +214,6 @@ static void bookkeeping_best_path(uint16_t last_level, const uint8_t *block) {
             break;
         }
         node->useless = 1; // it is useless.
-        if (node->sequence_length > 1 && !node->is_RLE) {
-            uint32_t freq, node_id;
-            uint32_t index = seq_freq_get_with_index(&block[node->offset], node->sequence_length, &freq, &node_id);
-            if (index != UINT32_MAX && freq > 0) {
-                seq_freq_set_existing(index, freq - 1, node->node_id);
-            }
-        }
     }
     // only nodes in the best path are marked useful
     for (uint32_t i = 0; i <= path_state.path_size[PATH_BEST]; i++) {
@@ -229,13 +222,6 @@ static void bookkeeping_best_path(uint16_t last_level, const uint8_t *block) {
         printf("\n Marking useful %u\n", node->node_id);
 #endif
         node->useless = 0;
-        if (node->sequence_length > 1 && !node->is_RLE) {
-            uint32_t freq, node_id;
-            uint32_t index = seq_freq_get_with_index(&block[node->offset], node->sequence_length, &freq, &node_id);
-            if (index != UINT32_MAX) {
-                seq_freq_set_existing(index, freq + 1, node->node_id);
-            }
-        }
     }
 }
 /**
